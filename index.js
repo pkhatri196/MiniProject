59c3c946e69a0a97244bf62cd438522b42a0d39c
pkhatri196 <payal.khatri02@gmail.com>
2022-01-15 22:05:48 +0530

    taskCards

---
+++ index.js
@@ -0,0 +1,47 @@
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+
+const newTask = 
+    ({id,
+    taskTitle,
+    taskDescription,
+    targetDate,
+    priority}) => `
+<div class="col-md-4" id=${id}>
+          <div class="logout shadow mx-auto mt-4"  style="max-width: 18rem;">
+            <div class="card-header">
+              <button type="button" class="btn btn-outline-dark">
+                <i class="fas fa-check"></i>
+              </button>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">${taskTitle}</h5>
+              <p class="card-text">${taskDescription}</p>
+
+              <button type="button" class="btn btn-outline-dark showhim">
+               <i class="far fa-calendar"></i>
+                  <div class="showme">${targetDate}</div>
+                </button>
+              <button type="button" class="btn btn-outline-dark showhim">
+                <i class="fas fa-exclamation-triangle"></i>
+                <div class="showme">${priority}</div>
+              </button>
+            </div>
+         </div>
+`
+
+
+const done = () =>{
+    const taskData = {
+        id:`${Date.now()}`, //unique number for each task
+        taskTitle: document.getElementById("title").value,
+        taskDescription: document.getElementById("description").value,
+        assignedTo:document.getElementById("person").value,
+        targetDate:document.getElementById("target date").value,
+        priority:document.getElementById("priority").value,
+
+    };
+    
+    const assignNewTask = newTask(taskData);
+    taskContainer.insertAdjacentHTML("beforeend",assignNewTask);
+};
